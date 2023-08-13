4c239da114052fe0e2808ce1cba941a6c6eefe4b
MHervian <mhervian0@gmail.com>
2023-08-13 20:09:52 +0700

    Update

--- storegg/bwa-storegg-main/components/organisms/FAQ/index.tsx
+++ storegg/bwa-storegg-main/components/organisms/FAQ/index.tsx
@@ -8,7 +8,7 @@ export default function FAQ() {
                     <div className="mb-4">
                         <p>
                             <a className="btn d-block text-start fs-5" data-bs-toggle="collapse" href="#faq1" role="button" aria-controls="collapseFAQ1">
-                                &#187; Bagaimana Cara Melakukan Pembayaran Di Itemku dengan Menggunakan BNI?
+                                &#187; Bagaimana Cara Melakukan Pembayaran Di Itemku dengan Menggunakan BNI, BCA, dan MANDIRI?
                             </a>
                         </p>
                         <div className="collapse" id="faq1">
@@ -32,60 +32,50 @@ export default function FAQ() {
                     <div className="mb-4">
                         <p>
                             <a className="btn d-block text-start fs-5" data-bs-toggle="collapse" href="#faq2" role="button" aria-controls="collapseFAQ1">
-                                &#187; Bagaimana Cara Melakukan Pembayaran Di Itemku dengan Menggunakan OVO?
+                                &#187; Bagaimana Cara Melakukan Pembayaran Di Itemku dengan Menggunakan Alfamart?
                             </a>
                         </p>
                         <div className="collapse" id="faq2">
                             <div className="card card-body">
-                                &#187; Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo saepe corporis consequuntur sint quas porro, quaerat deleniti neque, illum sunt reprehenderit praesentium culpa obcaecati quae. Quod quasi accusamus libero quam.
+                                <ol>
+                                    <li>User resgister akun di website storegg</li>
+                                    <li>User melakukan login</li>
+                                    <li>User klik Games yang dipilih</li>
+                                    <li>User input ID dari akun mobile legend</li>
+                                    <li>User memilih diamond yang ingin di top up</li>
+                                    <li>User memilih metode pembayaran</li>
+                                    <li>User input Account Number</li>
+                                    <li>User upload form bukti pembayaran Alfamart</li>
+                                    <li>User klick confirm payment</li>
+                                    <li>User melakukan confirm payment ke Admin by Whatsapp</li>
+                                    <li>User ke halaman dashboard melihat status pembayaran</li>
+                                    <li>Dan diamond berhasil bertambah di App Mobile Legend</li>
+                                </ol>
                             </div>
                         </div>
                     </div>
                     <div className="mb-4">
                         <p>
                             <a className="btn d-block text-start fs-5" data-bs-toggle="collapse" href="#faq3" role="button" aria-controls="collapseFAQ1">
-                                &#187; Bagaimana Cara Melakukan Pembayaran Itemku Dengan GoPay?
+                                &#187; Bagaimana Cara Melakukan Pembayaran Itemku Dengan INDOMARET?
                             </a>
                         </p>
                         <div className="collapse" id="faq3">
                             <div className="card card-body">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo saepe corporis consequuntur sint quas porro, quaerat deleniti neque, illum sunt reprehenderit praesentium culpa obcaecati quae. Quod quasi accusamus libero quam.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mb-4">
-                        <p>
-                            <a className="btn d-block text-start fs-5" data-bs-toggle="collapse" href="#faq4" role="button" aria-controls="collapseFAQ1">
-                                &#187; Bagaimana Cara Melakukan Pembayaran Itemku Dengan DANA?
-                            </a>
-                        </p>
-                        <div className="collapse" id="faq4">
-                            <div className="card card-body">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo saepe corporis consequuntur sint quas porro, quaerat deleniti neque, illum sunt reprehenderit praesentium culpa obcaecati quae. Quod quasi accusamus libero quam.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mb-4">
-                        <p>
-                            <a className="btn d-block text-start fs-5" data-bs-toggle="collapse" href="#faq5" role="button" aria-controls="collapseFAQ1">
-                                &#187; Bagaimana Cara Melakukan Pembayaran Di Itemku dengan Menggunakan ShopeePay?
-                            </a>
-                        </p>
-                        <div className="collapse" id="faq5">
-                            <div className="card card-body">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo saepe corporis consequuntur sint quas porro, quaerat deleniti neque, illum sunt reprehenderit praesentium culpa obcaecati quae. Quod quasi accusamus libero quam.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mb-4">
-                        <p>
-                            <a className="btn d-block text-start fs-5" data-bs-toggle="collapse" href="#faq6" role="button" aria-controls="collapseFAQ1">
-                                &#187; Bagaimana Cara Melakukan Pembayaran Di Itemku dengan Menggunakan LinkAja?
-                            </a>
-                        </p>
-                        <div className="collapse" id="faq6">
-                            <div className="card card-body">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo saepe corporis consequuntur sint quas porro, quaerat deleniti neque, illum sunt reprehenderit praesentium culpa obcaecati quae. Quod quasi accusamus libero quam.
+                                <ol>
+                                    <li>User resgister akun di website storegg</li>
+                                    <li>User melakukan login</li>
+                                    <li>User klik Games yang dipilih</li>
+                                    <li>User input ID dari akun mobile legend </li>
+                                    <li>User memilih diamond yang ingin di top up</li>
+                                    <li>User memilih metode pembayaran </li>
+                                    <li>User input Account Number</li>
+                                    <li>User upload form bukti pembayaran Indomaret </li>
+                                    <li>User klick confirm payment</li>
+                                    <li>User melakukan confirm payment ke Admin by Whatsapp</li>
+                                    <li>User ke halaman dashboard melihat status pembayaran</li>
+                                    <li>Dan diamond berhasil bertambah di App Mobile Legend</li>
+                                </ol>
                             </div>
                         </div>
                     </div>
